222cf9cdfba9b855f7343696a440c8fc1d3b5c1a
ersurya2018 <88522030+ersurya2018@users.noreply.github.com>
2021-08-14 12:23:08 +0530

    change in index page

--- Index.php
+++ Index.php
@@ -84,8 +84,114 @@
         </div>
         <!-- Container end -->
     </section>
-    <!-- Call to action end -->
-    <!--   Courses Details  -->
+    <section id="ts-features" class="ts-features">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="intro-feature">
+                        <h2>Welcome To Yes Computer Center</h2>
+                        <h4 class="text-justify">Yes computer center is an ISO 9001:2015 Certified IT training institute . We provide best computer training in software, hardware and multimedia fields. Our courses are specially designed and updated as per the current market needs as well as different student level which makes us a best it training institute in up.</h4>
+                        <p><a class="intro-link" href="aboutYCC.aspx"><i class="fa fa-caret-right"></i>More Information</a></p>
+                        <div class="img-box">
+                            <div class="img-box-small">Pillars of Excellence</div>
+                            <figure>
+                                <img class="img-responsive" src="images/banner/1.jpg" alt="">
+                            </figure>
+                            <figure>
+                                <img class="img-responsive" src="images/banner/3.jpg" alt="">
+                            </figure>
+                            <figure>
+                                <img class="img-responsive" src="images/banner/2.jpg" alt="">
+                            </figure>
+                        </div>
+                    </div>
+                    <!-- Intro box end -->
+                </div>
+                <!-- Col end -->
+                <div class="col-sm-6">
+                    <div class="featured-tab">
+                        <ul class="nav nav-tabs">
+                            <li class="active">
+                                <a class="animated fadeIn" href="#tab_c" data-toggle="tab">
+                                    <span class="tab-head">
+                                        <span class="tab-text-title">DIRECTOR'S MESSAGE</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="animated fadeIn" href="#tab_a" data-toggle="tab">
+                                    <span class="tab-head">
+                                        <span class="tab-text-title">Vision</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="animated fadeIn" href="#tab_b" data-toggle="tab">
+                                    <span class="tab-head">
+                                        <span class="tab-text-title">Mission </span>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active animated fadeInLeft" id="tab_c">
+                                <img src="images/intro/director.jpeg" class="pull-left" alt="" />
+                                <div class="ts-service-box-content">
+                                    <h4>DIRECTOR'S MESSAGE </h4>
+                                </div>
+                                <h5 class="text-justify">I take the pleasure in welcoming you to yes computer center, an institute dedicated to the proper growth and development of each and every student for a better career prospect.</h5>
+                                <br />
+                                <p class="text-justify">
+                                    The institute is all set to bring out the potential within each student and provide proper guidance so that their potentials can be utilized to make them future engineers capable of meeting any challenge that will be faced by them after leaving the institute. The college provides full support to improve their communication skills, critical thinking abilities, moral values and sense of responsibility. All efforts are made to improve the creativity and problem solving abilities of the students so that they can contribute their best to the society and the country.
+                           <br />
+                                    The infrastructure has been developed with most modern equipment; provisions have also been made to upgrade the same keeping pace with the technological advancements of the time.
+                                </p>
+                                <p class="text-justify">
+                                    YCC has a team of highly qualified, experienced and dedicated faculty who devote their time not only to teaching but also for the overall development of the students. Further, faculty from other renowned institutions are invited to deliver guest lectures to help augment their studies beyond syllabus. We are confident that the students passing out from this college will be technically sound, confident and have good leadership skills.
+                                </p>
+                                <b style="float: right">I wish all the best to all the prospective students.”</b>
+                            </div>
+                            <!-- Tab pane 1 end -->
+                            <div class="tab-pane animated fadeInRight" id="tab_b">
+                                <img class="pull-left" src="images/intro/mission.jpg" alt="" />
+                                <div class="ts-service-box-content">
+                                    <h4>Mission</h4>
+                                </div>
+                                <p class="text-justify">
+                                    <b>To provide innovation and excellence in academic design, delivery and assessment to ensure holistic development of students for employability, entrepreneurship, and higher education
+                           To design and keep the curricula updated, based on changing needs of industry and society worldwide, and to provide experiential learning through industry connect
+                           To be at the forefront of emerging technological research, innovation and creation of intellectual property to attract talent</b>
+                                    <br />
+                                    To retain talent by building relationships based on professionalism, mutual respect, accountability, engagement and integrity
+                           To leverage alumni to inculcate leadership skills, social awareness and a passion for lifelong learning to make students socially-responsible global citizens
+                           To build and maintain world-class infrastructure, and adopt modern automation technologies for the purpose of organizational efficiency
+                           To identify alternate sources of revenue and augment inflows
+                                </p>
+                            </div>
+                            <!-- Tab pane 2 end -->
+                            <div class="tab-pane animated fadeInRight" id="tab_a">
+                                <img class="pull-left" src="images/intro/vision.jpg" alt="" />
+                                <div class="ts-service-box-content">
+                                    <h4>Vision</h4>
+                                </div>
+                                <p class="text-justify">
+                                    <b>To be a globally recognized institute committed to excellence in academics, research, knowledge creation and delivery to develop socially-responsible professionals.
+                           And Our Vision is to see INDIA in the list of Developed Countries and expert in the field of Computer.</b>
+                                </p>
+                                <img class="img-thumbnail" src="images/intro/homebanner1.jpg" alt="" />
+                            </div>
+                            <!-- Tab pane 3 end -->
+                        </div>
+                        <!-- tab content -->
+                    </div>
+                    <!-- Featured tab end -->
+                </div>
+                <!-- Col end -->
+            </div>
+            <!-- Row end -->
+        </div>
+        <!-- Container end -->
+    </section>
     <!-- End main div -->
     <!-- footer -->
     <?php include 'includes/footer.php' ?>
